4f9b6b11b94990a5d98e318326058c18f1081451
Alexandr <alex.hadaev@n3wnormal.com>
2022-11-10 17:10:45 +0100

    config connect

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import "reflect-metadata";
 import express, {Application, Request, Response} from "express";
 import {sequelize} from "./src/db";
-// import cors from "cors"
+import cors from "cors"
 import router from "./src/routes";
 import errorHandler from './src/middleware/ErrorHandlingMiddleware'
 
@@ -10,8 +10,8 @@ dotenv.config();
 
 const app: Application = express();
 const PORT = process.env.PORT || 5000;
-console.log(process.env);
-// app.use(cors())
+
+app.use(cors())
 app.use(express.json());
 app.use('/', router)
 
--- src/db.ts
+++ src/db.ts
@@ -4,13 +4,55 @@ import {Color, Type} from './models/models';
 
 dotenv.config();
 
-export const sequelize = new Sequelize({
+function parseDatabaseUrl(url:any) {
+    let pattern = /^(?:([^:\/?#\s]+):\/{2})?(?:([^@\/?#\s]+)@)?([^\/?#\s]+)?(?:\/([^?#\s]*))?(?:[?]([^#\s]+))?\S*$/;
+    let matches: any =  url.match(pattern);
+    let params: any = {};
+    if (matches[5] != undefined) {
+        matches[5].split('&').map(function(x:string){
+            let a = x.split('=');
+            params[a[0]]=a[1];
+        });
+    }
+
+    return {
+        protocol: matches[1],
+        user: matches[2] != undefined ? matches[2].split(':')[0] : undefined,
+        password: matches[2] != undefined ? matches[2].split(':')[1] : undefined,
+        host: matches[3],
+        hostname: matches[3] != undefined ? matches[3].split(/:(?=\d+$)/)[0] : undefined,
+        port: matches[3] != undefined ? matches[3].split(/:(?=\d+$)/)[1] : undefined,
+        segments : matches[4] != undefined ? matches[4].split('/') : undefined,
+        params: params
+    };
+}
+
+
+interface ParsedDatabaseOptions {
+    protocol: string | undefined
+    host: string | undefined
+    username: string | undefined
+    password: string | undefined
+    database: string | undefined
+}
+
+const  optionsProduction = parseDatabaseUrl(process.env.DATABASE_URL)
+const optionsDevelopment = {
     database: process.env.DB_NAME,
-    dialect: 'postgres',
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST
+}
+console.log(optionsProduction);
+// Create conditional Sequelize database options here
+const sequelizeOptions = process.env.NODE_ENV === 'production'
+    ? optionsProduction
+    : optionsDevelopment
+
+export const sequelize = new Sequelize({
+    dialect: 'postgres',
     logging: false,
+    ...sequelizeOptions, // Spread options here
     models: [Color, Type],
     // storage: ':memory:',
     // models: [__dirname + '/models'], // or [Player, Team],
